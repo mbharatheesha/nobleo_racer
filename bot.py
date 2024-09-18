fd8a4b272e860f2d306ce1cbb951739302b065c6
Mukunda Bharatheesha <mukunda.bharatheesha@nobleo.nl>
2024-09-18 19:11:57 +0200

    Minimal, dumb, speed regulation

--- bot.py
+++ bot.py
@@ -6,14 +6,14 @@ from ...bot import Bot
 from ...linear_math import Transform
 
 
-class SimpleBot(Bot):
+class AutoSoori(Bot):
     @property
     def name(self):
-        return "SimpleBot"
+        return "AutoSoori"
 
     @property
     def contributor(self):
-        return "Nobleo"
+        return "Mukunda"
 
     def compute_commands(self, next_waypoint: int, position: Transform, velocity: Vector2) -> Tuple:
         target = self.track.lines[next_waypoint]
@@ -23,11 +23,14 @@ class SimpleBot(Bot):
         angle = target.as_polar()[1]
 
         # calculate the throttle
-        target_velocity = 50
+        target_velocity = 180
         if velocity.length() < target_velocity:
             throttle = 1
         else:
             throttle = -1
+
+        if abs(angle) > 20:
+            throttle = 0
 
         # calculate the steering
         if angle > 0:
